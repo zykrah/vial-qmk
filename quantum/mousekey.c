7157fb9ca1873ecc7fc94f204f7eb4e29ad25101
Drashna Jaelre <drashna@live.com>
2022-06-28 15:28:44 -0700

    [Bug] Fix issue with mousekey movement getting stuck (#17493)
    * [Bug] Fix issue with mousekey movement getting stuck
    
    * Lint

--- quantum/mousekey.c
+++ quantum/mousekey.c
@@ -75,6 +75,10 @@ uint8_t mk_wheel_interval = MOUSEKEY_WHEEL_INTERVAL;
 uint8_t mk_wheel_max_speed   = MOUSEKEY_WHEEL_MAX_SPEED;
 uint8_t mk_wheel_time_to_max = MOUSEKEY_WHEEL_TIME_TO_MAX;
 
+bool should_mousekey_report_send(report_mouse_t *mouse_report) {
+    return mouse_report->x || mouse_report->y || mouse_report->v || mouse_report->h;
+}
+
 #    ifndef MK_COMBINED
 #        ifndef MK_KINETIC_SPEED
 
@@ -258,7 +262,7 @@ void mousekey_task(void) {
         }
     }
 
-    if (has_mouse_report_changed(&mouse_report, &tmpmr)) {
+    if (has_mouse_report_changed(&mouse_report, &tmpmr) || should_mousekey_report_send(&mouse_report)) {
         mousekey_send();
     }
     memcpy(&mouse_report, &tmpmr, sizeof(tmpmr));
@@ -380,7 +384,7 @@ void mousekey_task(void) {
         mouse_report.h = tmpmr.h;
     }
 
-    if (has_mouse_report_changed(&mouse_report, &tmpmr)) {
+    if (has_mouse_report_changed(&mouse_report, &tmpmr) || should_mousekey_report_send(&mouse_report)) {
         mousekey_send();
     }
     memcpy(&mouse_report, &tmpmr, sizeof(tmpmr));
