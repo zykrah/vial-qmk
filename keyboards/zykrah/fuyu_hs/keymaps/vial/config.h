e431e8b8faffa3465e22dee104d89aae7a99bec1
zykrah <23428162+zykrah@users.noreply.github.com>
2023-08-13 00:12:26 +1000

    Fuyu HS

---
+++ keyboards/zykrah/fuyu_hs/keymaps/vial/config.h
@@ -0,0 +1,25 @@
+/*
+Copyright 2022 Zykrah
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB5, 0x71, 0x97, 0x16, 0xCD, 0xFA, 0xDF, 0x3B}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 12}
+
+#define ENABLE_UNDERGLOW
